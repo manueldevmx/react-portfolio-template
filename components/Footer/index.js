191b1ec0c98c8bc001720a2e67b59d0dcd6e0141
manueldevmx <manueldevmx@gmail.com>
2022-07-25 00:31:50 -0500

    mailto

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -13,7 +13,7 @@ const Footer = () => {
             </div>
             <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
                 Made With ❤ by{" "}
-                <Link href="http://www.chetanverma.com">
+                <Link href="mailto:mkt.alann@gmail.com">
                     <a className="underline underline-offset-1">Alan De León</a>
                 </Link>
             </h1>
